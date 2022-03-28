d50636df038bc214a449c7650768a1b1311fc8a2
pRob3 <bubblan@slaf.se>
2022-03-28 06:40:35 +0200

    Alert Component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import NotFound from './pages/NotFound'
-import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
+import Navbar from './components/layout/Navbar'
+import Alert from './components/layout/Alert'
 import { GithubProvider } from './context/github/GithubContext'
 import { AlertProvider } from './context/alert/AlertContext'
 
@@ -15,6 +16,7 @@ function App() {
           <div className='flex flex-col justify-between h-screen'>
             <Navbar />
             <main className='container mx-auto px-3 pb-12'>
+              <Alert />
               <Routes>
                 <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
